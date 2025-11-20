982020bb371cc19fe74b6cdc8832233e9c130b53
simoon <420526391@qq.com>
2025-11-20 14:52:15 +0800

    修改config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.js
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  server: {
+    host: '0.0.0.0', // 允许外部访问
+    port: 3000        // 默认端口 3000
+  }
 })
